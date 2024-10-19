fb67666a1a9c8e3da66678f67d942f604015de08
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-10-19 21:46:02 +0100

    directus

--- frontend/server/api/me.get.ts
+++
@@ -1,6 +0,0 @@
-import { readMe, withToken } from "@directus/sdk";
-
-export default defineEventHandler((event) => {
-  const cookie = getCookie(event, "directusToken");
-  return useDirectus().request(withToken(cookie!, readMe()));
-});
--- frontend/server/utils/useDirectus.ts
+++
@@ -1,7 +0,0 @@
-import { authentication, createDirectus, rest } from "@directus/sdk";
-
-export default function useDirectus() {
-  return createDirectus("http://127.0.0.1:8055")
-    .with(authentication("json"))
-    .with(rest());
-}
